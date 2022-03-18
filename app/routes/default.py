9e008def5232571999feba904fcb5bbdafa016ec
CyberWarrior2099 <s_alexander.krull@ousd.org>
2022-03-18 09:34:59 -0700

    Merge branch 'master' of https://github.com/CyberWarrior2099/CapFlaskTemplate

--- app/routes/default.py
+++ app/routes/default.py
@@ -6,12 +6,6 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-<<<<<<< HEAD
 @app.route('/about')
 def about():
     return render_template('about.html')
-=======
-@app.route('/aboutus')
-def aboutus():
-    return render_template('aboutus.html')
->>>>>>> a08386a94239d8d234ae0d5a1ef66aa9d43ea93c
